5e1dc6e1957274db21aacfb1ea96242865f6ec94
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-03-20 17:22:33 +0100

    OMT-149 (claim in new tabs) - naming tabs side effect

--- src/pages/ClaimBatchPage.js
+++ src/pages/ClaimBatchPage.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withTheme, withStyles } from "@material-ui/core/styles";
+import { injectIntl } from 'react-intl';
 import BatchRunLauncher from "../components/BatchRunLauncher";
 import BatchRunSearcher from "../components/BatchRunSearcher";
 import AccountPreviewer from "../components/AccountPreviewer";
+import { formatMessage } from "@openimis/fe-core";
 import { RIGHT_PROCESS, RIGHT_FILTER, RIGHT_PREVIEW } from "../constants";
 
 const styles = theme => ({
@@ -13,6 +15,11 @@ const styles = theme => ({
 });
 
 class ClaimBatchPage extends Component {
+
+    componentDidMount() {
+        document.title = formatMessage(this.props.intl, "claim_batch", "claimBatch.page.title")
+    }
+
     render() {
         const { rights, classes } = this.props;
         if (!rights.filter(r => r >= RIGHT_PROCESS && r <= RIGHT_PREVIEW).length) return null;
@@ -36,4 +43,4 @@ const mapStateToProps = state => ({
     rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
 });
 
-export default withTheme(withStyles(styles)(connect(mapStateToProps)(ClaimBatchPage)));
+export default injectIntl(withTheme(withStyles(styles)(connect(mapStateToProps)(ClaimBatchPage))));
